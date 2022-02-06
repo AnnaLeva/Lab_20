7c3fb564660c5154fdcf62b929c79ac6aec15840
Анастасия Александровна Ковалева <309755@niuitmo.ru>
2022-02-07 02:14:54 +0300

    Добавьте файлы проекта.

---
+++ Lab_20/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_20
+{
+    class Program
+    {
+        delegate int MyDelegate(int a, int b);
+        static void Main(string[] args)
+        {
+            MyDelegate myDelegate = PowerAB;
+            int r = myDelegate(2, 10);
+            Console.WriteLine(r);
+
+            myDelegate = PowerBA;
+            r = myDelegate(2, 10);
+            Console.WriteLine(r);
+
+            Console.ReadKey();
+        }
+    
+        static int PowerAB (int a, int b) => (int)Math.Pow(a, b);
+        static int PowerBA (int a, int b) => (int)Math.Pow(b, a);
+       
+    }
+}
